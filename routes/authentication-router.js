ff8e0d4ffe170d8c41bd274a9acc61f3bb84acf3
KieranVieira <kieranvieira@live.com>
2019-02-27 16:19:56 -0500

    Setup database and jwt

--- routes/authentication-router.js
+++ routes/authentication-router.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const db = require('../database/dbConfig');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const generateToken = user => {
+    const payload = {
+        subject: user.id,
+        username: user.username
+    }
+
+    const options = {
+        expiresIn: '1d',
+    }
+
+    jwt.sign(payload, secret, options)
+
+}
+
+router.post('/register', (req, res) => {
+    res.send('this is a route')
+})
+
+router.post('/login', (req, res) => {
+    res.send('this is a route')
+})
+
+router.get('/users', (req, res) => {
     res.send('this is a route')
 })
 
